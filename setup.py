9da39de5d426bcb5168f0d7e4a5351b358eede94
Manuel Barkhau <mbarkhau@gmail.com>
2018-05-24 12:48:16 +0200

    fix py27

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import unicode_literals
+# This file is part of markdown-aafigure.
+# https://github.com/mbarkhau/markdown-aafigure
+# (C) 2018 Manuel Barkhau <mbarkhau@gmail.com>
+#
+# SPDX-License-Identifier:    MIT
 
 import os
 import setuptools
